f0adc12ef0456de62c9070f28d87e2c513926bf1
Hadas Neuman <hadas.doron@gmail.com>
2022-12-27 14:40:48 +0200

    Updatig the Python script and plots

--- ex2.py
+++ ex2.py
@@ -5,6 +5,9 @@
 import numpy as np
 
 
+# Classes
+##############
+
 class Ols(object):
     """ An implementation on ordinary least squares"""
 
@@ -13,75 +16,34 @@ class Ols(object):
 
     @staticmethod
     def pad(X):
-        print('----- Pad -------')
-        print('X.shape', X.shape)
         X = np.c_[X, np.ones(X.shape[0])]
-        print('X.shape', X.shape)
         return X
 
     def fit(self, X, Y):
-        # remeber pad with 1 before fitting
+        # remember pad with 1 before fitting
         # Update the weight
-        X = Ols.pad(X)
-        self.w = self._derive(X, Y)
-        print('------- Fit --------')
-        print('self.w.shape', self.w.shape)
-        print('X.shape', X.shape)
+        self.w = self._fit(X, Y)
 
     @staticmethod
-    def _derive(X, Y):
+    def _fit(X, Y):
+        X = Ols.pad(X)
         w_ols = np.dot(np.dot(np.linalg.pinv(np.dot(X.T, X)), X.T), Y)
-        print('w_ols.shape:', w_ols.shape)
-        print('Y.shape', Y.shape)
         return w_ols
-
-    def _fit(self, X, Y):
-        # optional to use this
-        pass
 
     def predict(self, X):
         # return wx
-        print('------- Predict --------')
-        print('self.w.shape', self.w.shape)
-        print('X.shape', X.shape)
-        X = Ols.pad(X)
-        print('X.shape', X.shape)
-        pred_y = np.dot(X, self.w)
-        print('pred_y.shape:', pred_y.shape)
+        pred_y = self._predict(X)
         return pred_y
 
     def _predict(self, X):
-        # optional to use this
-        pass
+        X = self.pad(X)
+        pred_y = np.dot(X, self.w)
+        return pred_y
 
     def score(self, X, Y):
-        pred_y = self.predict(X)
-        mse = np.mean((pred_y - Y)**2)
+        pred_y = self._predict(X)
+        mse = np.mean((pred_y - Y) ** 2)
         return mse
-
-
-# TESTING
-n, k = 100000, 2
-# set the dimensions of the design matrix
-beta = np.array([1, 1, 10])  # set the true coefficients
-x = np.concatenate([np.ones((n, 1)), np.random.randn(n, k)], axis=1)  # generate random x
-y = np.matmul(x, beta) + np.random.randn(n)  # generate random y
-
-print('Inputs:')
-print('x:', x.shape)
-print('y:', y.shape)
-
-ols_obj = Ols()
-ols_obj.fit(x, y)
-
-
-predicted_y = ols_obj.predict(x)
-#print('Pred y:\n', predicted_y)
-#print('true y:\n', y)
-#print('True Y - Pred Y:\n', y-predicted_y)
-
-omse = ols_obj.score(x, y)
-print('MSE:', omse)
 
 
 # Write a new class OlsGd which solves the problem using gradinet descent. The class should get as a parameter the
@@ -89,16 +51,21 @@ print('MSE:', omse)
 # respect to number of iterations. What is the effect of learning rate? How would you find number of iteration
 # automatically? Note: Gradient Descent does not work well when features are not scaled evenly (why?!). Be sure to
 # normalize your feature first.
-class Normalizer():  ## For the GD
+class Normalizer:
     def __init__(self):
-        pass
+        self.means = None
+        self.stds = None
 
     def fit(self, X):
-        pass
+        self.means = np.mean(X, axis=0)  # The mean of each column (=feature)
+        self.stds = np.std(X, axis=0)  # The STD of each column (=feature)
 
     def predict(self, X):
-        # apply normalization
-        pass
+        norm_x = (X - self.means) / self.stds
+        return norm_x
+
+    def __str__(self):
+        return f'Normalizer: means: {self.means} stds: {self.stds}'
 
 
 class OlsGd(Ols):
@@ -108,6 +75,7 @@ class OlsGd(Ols):
                  normalize=True,
                  early_stop=True,
                  verbose=True):
+
         super(OlsGd, self).__init__()
         self.learning_rate = learning_rate
         self.num_iteration = num_iteration
@@ -117,23 +85,131 @@ class OlsGd(Ols):
         self.verbose = verbose
 
     def _fit(self, X, Y, reset=True, track_loss=True):
-        # remeber to normalize the data before starting
-        pass
+        # remember to normalize the data before starting
+        X0 = X  # Saving the original X
+        if self.normalize:
+            self.normalizer.fit(X)
+            X = self.normalizer.predict(X)
+        pad_X = self.pad(X)
+        self.w = np.random.randint(100, size=pad_X.shape[1])  # Random initial weights
+        w0 = np.random.randint(100, size=pad_X.shape[1])  # Random initial weights
+        i = 0
+        while not self._should_stop_func(i, w0):
+            w0 = self.w  # The previous step w, for early stop
+            self._step(pad_X, Y)
+            i += 1
+            if self.verbose:
+                loss = self.score(X0, Y)  # Computing the loss
+                print(f'Iteration {i} error: {loss}')
+        if self.verbose:
+            print(f'Final step: iteration: {i} Error: {self.score(X0, Y)}')
+        return self.w
+
+    def _derive(self, X, Y):
+        """ Derives the w """
+        n = X.shape[0]
+        grad_x = (2 / n) * np.dot(X.T, (np.dot(X, self.w) - Y))
+        return grad_x
 
     def _predict(self, X):
-        # remeber to normalize the data before starting
-        pass
+        if self.normalize:
+            X = self.normalizer.predict(X)
+        # pred_y = np.dot(X, self.w)
+        pred_y = super()._predict(X)
+
+        return pred_y
 
     def _step(self, X, Y):
         # use w update for gradient descent
-        pass
+        grad_x = self._derive(X, Y)
+        self.w = self.w - np.dot(self.learning_rate, grad_x)
+
+    def _should_stop_func(self, current_step, w0, delta=0.001):
+        """Returns true if the function should stop"""
+        should_stop = False
+        if current_step >= self.num_iteration:
+            should_stop = True
+        elif self.early_stop and (abs(self.w - w0) <= delta).all():
+            should_stop = True
+        return should_stop
 
 
 class RidgeLs(Ols):
     def __init__(self, ridge_lambda, *wargs, **kwargs):
-        super(RidgeLs, self).__init__(*wargs, **kwargs)
+        super(RidgeLs, self).__init__()
         self.ridge_lambda = ridge_lambda
 
     def _fit(self, X, Y):
         # Closed form of ridge regression
-        pass
+        X = self.pad(X)
+        in_brackets = np.dot(X.T, X) + np.dot(self.ridge_lambda, np.identity(X.shape[1]))
+        w_ridge = np.dot(np.dot(np.linalg.pinv(in_brackets), X.T), Y)
+        return w_ridge
+
+
+class RidgeGd(OlsGd):
+    def __init__(self, ridge_lambda, *wargs, **kwargs):
+        super(RidgeGd, self).__init__(*wargs, **kwargs)
+        self.ridge_lambda = ridge_lambda
+
+    def _derive(self, X, Y):
+        """ Derives the w """
+        grad_x = super()._derive(X, Y) + self.ridge_lambda * self.w
+        return grad_x
+
+
+# Testing
+##############
+
+if __name__ == "__main__":
+    from sklearn.datasets import load_boston
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LinearRegression
+    from sklearn.linear_model import Lasso
+    from sklearn.linear_model import Ridge
+    from sklearn.metrics import mean_squared_error
+
+    all_X, all_y = load_boston(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(all_X, all_y, test_size=0.25)
+
+    regression_methods = [Ols, OlsGd, RidgeLs, RidgeGd]
+    # Ols
+    print('Ols:')
+    model = Ols()
+    model.fit(X_train, y_train)
+    sk_model = LinearRegression()
+    sk_model.fit(X_train, y_train)
+    print('Home model:\t', model.score(X_test, y_test))
+    print('SK model:\t', mean_squared_error(y_test, sk_model.predict(X_test)))
+    print('-' * 30)
+
+    # OlsGd
+    print('OlsGd:')
+    model = OlsGd(verbose=False, learning_rate=0.1)
+    model.fit(X_train, y_train)
+    print('Home model:\t', model.score(X_test, y_test))
+    print('SK model:\t', mean_squared_error(y_test, sk_model.predict(X_test)))
+    print('-' * 30)
+
+    # RidgeLs
+    print('RidgeLs:')
+    model = RidgeLs(ridge_lambda=0.01)
+    model.fit(X_train, y_train)
+    sk_model = Ridge()
+    sk_model.fit(X_train, y_train)
+    print('Home model:\t', model.score(X_test, y_test))
+    print('SK model:\t', mean_squared_error(y_test, sk_model.predict(X_test)))
+    print('-' * 30)
+
+    # RidgeGd
+    print('RidgeGd:')
+    model = RidgeGd(verbose=False, learning_rate=0.1, ridge_lambda=0.01)
+    model.fit(X_train, y_train)
+    print('Home model:\t', model.score(X_test, y_test))
+    print('-' * 30)
+
+    # Lasso
+    print('Lasso:')
+    sk_model = Lasso()
+    sk_model.fit(X_train, y_train)
+    print('SK model:\t', mean_squared_error(y_test, sk_model.predict(X_test)))
